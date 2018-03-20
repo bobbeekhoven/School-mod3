be651585976b93d1984d477d4ccc7a1d26a4ee3b
bobbeekhoven <bob.vanbeekhoven@itvitaelearning.nl>
2018-03-20 14:33:03 +0100

    Add files via upload

---
+++ Exercise 3/GradesPrototype/Views/LogonPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+
+namespace GradesPrototype.Views
+{
+    /// <summary>
+    /// Interaction logic for LogonPage.xaml
+    /// </summary>
+    public partial class LogonPage : UserControl
+    {
+        public LogonPage()
+        {
+            InitializeComponent();
+        }
+
+        #region Event Members
+        public event EventHandler LogonSuccess;
+        public event EventHandler LogonFailed;
+        // Exercise 3: Task 1a: Define LogonFailed event
+
+        #endregion
+
+        #region Logon Validation
+
+        // Exercise 3: Task 1b: Validate the username and password against the Users collection in the MainWindow window
+        private void Logon_Click(object sender, RoutedEventArgs e)
+        {
+            //Find user and check if it's a teacher
+            var teacher = (from Teacher t in DataSource.Teachers
+                           where String.Compare(t.UserName, username.Text) == 0
+                           && String.Compare(t.Password, password.Password) == 0
+                           select t).FirstOrDefault();
+
+            if (!String.IsNullOrEmpty(teacher.UserName))
+            {
+                // Save user ID and role
+                SessionContext.UserID = teacher.TeacherID;
+                SessionContext.UserRole = Role.Teacher;
+                SessionContext.UserName = teacher.UserName;
+                SessionContext.CurrentTeacher = teacher;
+
+                LogonSuccess(this, null);
+                return;
+            }
+            // If not a teacher check if it's a student
+            else
+            {
+                var student = (from Student s in DataSource.Students
+                               where String.Compare(s.UserName, username.Text) == 0
+                               && String.Compare(s.Password, password.Password) == 0
+                               select s).FirstOrDefault();
+
+                if (!String.IsNullOrEmpty(student.UserName))
+                {
+                    // Save user ID and role
+                    SessionContext.UserID = student.StudentID;
+                    SessionContext.UserRole = Role.Student;
+                    SessionContext.UserName = student.UserName;
+                    SessionContext.CurrentStudent = student;
+
+                    LogonSuccess(this, null);
+                    return;
+                }
+            }
+
+            // If nothing matches, raise LogonFailed event
+            LogonFailed(this, null);
+            return;
+        }
+        #endregion
+    }
+}
---
+++ Exercise 3/GradesPrototype/Views/StudentsPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GradesPrototype.Data;
+using GradesPrototype.Services;
+
+namespace GradesPrototype.Views
+{
+    /// <summary>
+    /// Interaction logic for StudentsPage.xaml
+    /// </summary>
+    public partial class StudentsPage : UserControl
+    {
+        public StudentsPage()
+        {
+            InitializeComponent();
+        }
+
+        #region Display Logic
+
+        // Exercise 3: Task 3a: Display students for the current teacher (held in SessionContext.CurrentTeacher )
+        public void Refresh()
+        {
+            ArrayList students = new ArrayList();
+            foreach (Student student in DataSource.Students)
+            {
+                if (student.TeacherID == SessionContext.CurrentTeacher.TeacherID)
+                {
+                    students.Add(student);
+                }
+            }
+            list.ItemsSource = students;
+
+            txtClass.Text = String.Format("Class {0}", SessionContext.CurrentTeacher.Class);
+        }
+        #endregion
+
+        #region Event Members
+        public delegate void StudentSelectionHandler(object sender, StudentEventArgs e);
+        public event StudentSelectionHandler StudentSelected;        
+        #endregion
+
+        #region Event Handlers
+
+        // Exercise 3: Task 3b: If the user clicks on a student, display the details for that student
+        private void Student_Click(object sender, RoutedEventArgs e)
+        {
+            Button itemClicked = sender as Button;
+            if (itemClicked != null)
+            {
+                int studentID = (int)itemClicked.Tag;
+                if (StudentSelected != null)
+                {
+                    Student student = (Student)itemClicked.DataContext;
+
+                    StudentSelected(sender, new StudentEventArgs(student));
+                }
+            }
+        }
+        #endregion
+    }
+
+    // EventArgs class for passing Student information to an event
+    public class StudentEventArgs : EventArgs
+    {
+        public Student Child { get; set; }
+
+        public StudentEventArgs(Student s)
+        {
+            Child = s;
+        }
+    }
+}
